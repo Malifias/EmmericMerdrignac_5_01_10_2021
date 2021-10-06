5d03b5ea3cd6f7862f1c5fd8d5498b8604651092
Malifias <emmeric8@gmail.com>
2021-10-06 14:57:58 +0200

    produc index

---
+++ front/js/index.js
@@ -0,0 +1,28 @@
+fetch('http://localhost:3000/api/products')
+    .then(function(res) {
+        if (res.ok) {
+        return res.json();
+        }
+    })
+    .then(function(value) {
+        console.log(value);
+        let productContaineur = document.getElementById('products');
+        value.forEach(product => {
+            productContaineur.innerHTML +=
+        `
+        <a href="./product.html?id=${product._id}">
+            <article>
+              <img src=${product.imageUrl} alt=${product.altTxt}>
+              <h3 class="productName">${product.name}</h3>
+              <p class="productDescription">${product.description}</p>
+            </article>
+          </a> `
+    })
+    console.log(productContaineur)
+        })
+        
+    .catch(function(err) {
+        // Une erreur est survenue
+    })
+    
+    

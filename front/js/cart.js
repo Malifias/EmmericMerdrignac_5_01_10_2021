6bbb68fd31f6f4a49a1f21b171fa2fd5c6cb9fef
Malifias <emmeric8@gmail.com>
2021-10-27 13:32:16 +0200

    delete working

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,5 +1,6 @@
 // Element HTML du cart
 const cartSection = document.getElementById("cart__items");
+
 // la fameuse fonction get cart qui recupere le panier.
 function getCart() {
   let items = [];
@@ -38,7 +39,7 @@ let cardsFetch = function () {
                 <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${items[i][2]}">
               </div>
               <div class="cart__item__content__settings__delete">
-                <p class="deleteItem">Supprimer</p>
+                <p class="deleteItem" onclick="deleteItem()">Supprimer</p>
               </div>
             </div>
           </div>
@@ -52,30 +53,44 @@ let cardsFetch = function () {
         // total Quantity
         qty += items[i][2];
         document.getElementById("totalQuantity").innerHTML = qty;
+
+        const suprimer = document.querySelectorAll("deleteItem");
+        suprimer.forEach((btn)=>
+        {
+          btn.addEventListener('click',e =>{
+            deleteItem()
+          })
+        })
       }
-    });
+      
+    }
+    );
+    
 };
 
 //fonction pour supprimer un item du storage et changer son HTML
 
-const article = document.getElementsByTagName("article");
-const deleteItem = document.getElementsByClassName("deleteItem");
-
-for (let i = 0; i < article.length; i++) {
-  deleteItem.addEventListener("click", () => {
-    let items = getCart();
-    let id = article[i].dataset.id;
-    let color = article[i].dataset.color;
-    for (let j = 0; j < items.length; j++) {
-      if (id == items[j][0] && color == items[j][1]) {
-        let deletedItem = items.splice(j, 1);
-        localStorage.setItem("panier", JSON.stringify(items));
-        document.getElementsByClass(
-          cart__item[i]
-        ).innerHTML = `<p>Kanap supprimé du panier !</p>`;
-      }
-    }
-  });
+function deleteItem() {
+  let items = JSON.parse(localStorage.getItem("panier"));
+  for (let i = 0; i < items.length; i++) {
+    let removedItem = items.splice(i, 1);
+    localStorage.setItem("panier", JSON.stringify(items));
+  }
 }
+const form = document.querySelector("#order");
+form.addEventListener('click', e => {
+  e.preventDefault();
+  
+  const contact = {
+    firstName: document.querySelector("#firstName").value,
+    lastName: document.querySelector("#lastName").value,
+    address: document.querySelector("#address").value,
+    city: document.querySelector("#city").value,
+    email: document.querySelector("#email").value
+  }
+  localStorage.setItem("formValues",JSON.stringify(contact));
+  console.log(contact)
+
+});
 
 cardsFetch();
---
+++ front/js/confirmation.js
@@ -0,0 +1,5 @@
+const contact = JSON.parse(sessionStorage.getItem("formValues"));
+console.log(contact)
+
+let orderId= document.getElementById("orderId");
+orderId.innerHTML = orderId.id;
